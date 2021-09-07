9e95fb5dd23ae6a256b32437f4a6c14f149dd9c0
VICTOR EJIKE NWOSU <74430629+Eprince-hub@users.noreply.github.com>
2021-09-07 14:37:00 +0200

    add all the needed packages

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # node-random-color-generator
+
 Second day
+
+Generate random colors
+
+when a user enter node index.js the result: random number;
--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Hello people');
+/* const printHashes = () => {
+	for (let rows = 1; rows <= 9; rows++) {
+		const hash = '#';
+		for (let column = 1; column <= 31; column++) {
+			process.stdout.write(hash);
+		}
+		console.log('');
+	}
+};
+printHashes();
+
+function getRandomColor() {
+	const letters = '0123456789ABCDEF';
+	let color = '#';
+	for (let i = 0; i < 6; i++) {
+		const randomHex = Math.floor(Math.random() * letters.length);
+		color += letters[randomHex];
+	}
+	console.log(color);
+}
+getRandomColor();
+ */
+
+console.log(process.argv);
