a74d1132429de0f1f537af772c64549ee65c50ef
VICTOR EJIKE NWOSU <74430629+Eprince-hub@users.noreply.github.com>
2021-09-08 09:09:08 +0200

    update index.js and removed unused dependencies

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const randomColor = require('randomcolor');
+const chalk = require('chalk');
+
 /* const printHashes = () => {
 	for (let rows = 1; rows <= 9; rows++) {
 		const hash = '#';
@@ -9,6 +12,8 @@
 };
 printHashes();
 
+
+//Function generating random Hex codes
 function getRandomColor() {
 	const letters = '0123456789ABCDEF';
 	let color = '#';
@@ -21,4 +26,104 @@ function getRandomColor() {
 getRandomColor();
  */
 
-console.log(process.argv);
+// Function that prints hashes
+function printHashes() {
+	const squareHashes = [];
+	let result;
+	for (let rows = 1; rows <= 9; rows++) {
+		result = '';
+		for (let cols = 1; cols <= 31; cols++) {
+			result += '#';
+		}
+		// push the printed hashes and spaces to the empty array!
+		squareHashes.push(result);
+	}
+	return squareHashes.join('\n');
+}
+
+// Function that prints random hex color codes!
+function getRandomColor() {
+	const letters = '0123456789ABCDEF';
+	let newColor = '#';
+	for (let i = 0; i < 6; i++) {
+		const randomHex = Math.floor(Math.random() * letters.length);
+		newColor += letters[randomHex];
+	}
+	return newColor;
+}
+
+// let currentColor = randomColor();
+// console.log(chalk.hex(currentHexColor)(hashes));
+
+// print random color of hashes when no argument is given!
+if (!process.argv[2]) {
+	const hashes = printHashes();
+	const currentHexColor = getRandomColor();
+	console.log(chalk.hex(currentHexColor)(hashes));
+
+	// if argument is given check if it is a number or not!
+} else if (process.argv.length >= 2) {
+	// if it is not a number trigger this statement
+	if (isNaN(process.argv[2]) && isNaN(process.argv[3])) {
+		console.log('Please enter a valid Number');
+	} else {
+		// if the given argument is a number, trigger this statement!
+		const width = process.argv[2];
+		const height = process.argv[3];
+		const luminosity = process.argv[4];
+		const hue = process.argv[5];
+
+		function userPrintedHashes() {
+			const squareHashes = [];
+			let result;
+			for (let rows = 1; rows <= height; rows++) {
+				result = '';
+				for (let cols = 1; cols <= width; cols++) {
+					result += '#';
+				}
+				// push the printed hashes and spaces to the empty array!
+				squareHashes.push(result);
+			}
+			return squareHashes.join('\n');
+		}
+
+		const hashesColor = randomColor({
+			luminosity: luminosity,
+			hue: hue,
+		});
+
+		const printedHashes = userPrintedHashes();
+		console.log(chalk.hex(hashesColor)(printedHashes));
+	}
+}
+
+/*
+
+	// If nothing is entered by the user then a random color will be generated.
+	if (!process.argv[2]) {
+		const hashes = printHashes();
+		const currentHexColor = getRandomColor();
+		console.log(chalk.hex(currentHexColor)(hashes));
+
+		// the first argument and the third argument is needed to fire this process!
+	} else if (process.argv[2] && process.argv[3]) {
+		const luminosity = process.argv[2];
+		const hue = process.argv[3];
+
+		// getting the hex color code for the entered luminosity and hue!
+		const hashesColor = randomColor({
+			luminosity: luminosity,
+			hue: hue,
+		});
+		const hashes = printHashes();
+		console.log(chalk.hex(hashesColor)(hashes));
+	} else {
+		const hue = process.argv[2];
+		const hashesColor = randomColor({
+			hue: hue,
+		});
+		const hashes = printHashes();
+		console.log(chalk.hex(hashesColor)(hashes));
+	}
+
+ */
